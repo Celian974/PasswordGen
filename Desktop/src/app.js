3039efbcbfb8b6cadd73d2d9243fbf96879a6477
Celian Siampirave <celian.siampirave@epitech.eu>
2025-06-11 15:57:47 +0200

    [FEAT] add JS app file

---
+++ Desktop/src/app.js
@@ -0,0 +1,17 @@
+const {app, BrowserWindow} = require('electron');
+
+const createWindow = () => {
+    const window = new BrowserWindow({
+        width: 800,
+        height: 600
+    });
+    window.loadFile('src/index.html');
+}
+
+app.whenReady().then(() => {
+    createWindow();
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit()
+});
